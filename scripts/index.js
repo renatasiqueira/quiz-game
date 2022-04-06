327d04156f9d6caa93eec0b72ad8f47bd6d2204a
unknown <renatabssiqueira@gmail.com>
2022-04-07 00:18:07 +0100

    Done

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,66 @@
 //Elementos do HTML
+const jogo = new Quiz;
 
-function buildQuiz() {
-    //Local para armazenar o output do HTML
+const pergunta = document.getElementById("pergunta");
+
+const op1 = document.getElementById("op1");
+
+const op2 = document.getElementById("op2");
+
+const op3 = document.getElementById("op3");
+
+const info = document.getElementById("info");
+
+const start = document.getElementById("start");
+
+start.addEventListener('click', () => {
+    printQuestions();   
+});
+
+op1.addEventListener('click', (event) => {
+    let resposta = event.target.innerText;
+    if (resposta === jogo.pegarPergunta().correct) {
+       info.innerText = "Você acertou!"; 
+    } else {
+        info.innerText = "Você errou!";
+    }
+})
+
+op2.addEventListener('click', (event) => {
+    let resposta = event.target.innerText;
+    if (resposta === jogo.pegarPergunta().correct) {
+       info.innerText = "Você acertou!"; 
+    } else {
+        info.innerText = "Você errou!";
+    }
+})
+
+op3.addEventListener('click', (event) => {
+    let resposta = event.target.innerText;
+    if (resposta === jogo.pegarPergunta().correct) {
+       info.innerText = "Você acertou!"; 
+    } else {
+        info.innerText = "Você errou!";
+    }
+})
+
+// MÉTODO PARA MUDAR PERGUNTA 
+
+console.log(jogo.pegarPergunta().correct);
+
+function printQuestions () {
+
+    pergunta.innerText = jogo.pegarPergunta().question;
+    op1.innerText = jogo.pegarPergunta().options[0];
+    op2.innerText = jogo.pegarPergunta().options[1];
+    op3.innerText = jogo.pegarPergunta().options[2];
+
+}
+
+
+
+   
+    /*//Local para armazenar o output do HTML
     const output = [];
 
     //Para contruir cada pergunta
@@ -38,5 +97,5 @@ function buildQuiz() {
 }
 
 function showResults() {
-    
-}
+ */   
+
--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,17 +1,31 @@
 //Este arquivo é onde vou criar a classe e acessar as minhas perguntas 
+//Montar o quiz 
+
+
 console.log(perguntas)
 class Quiz {
 
     constructor() {
         this.scores = 0;
+        this.questions = [...perguntas];
+        this.perguntaAtual = 0;
+
+        this.result = "";
+
     }
 
     start() {
-      //primeira pergunta e primeira responta
+    console.log('estou na função start');
       
     }
 
+    pegarPergunta() {
+     return this.questions[this.perguntaAtual];
+    }
 
+    resultado(){
+      return this.result[this.correct];
+    }
 
 
 
